652612972b617c26879f6638bb3b64b1296e524b
1-Soul-1 <ruslanpupkin789@gmail.com>
2026-01-27 11:07:32 +0500

    settings

---
+++ config/conftest.py
@@ -0,0 +1,29 @@
+import pytest
+from library.models import Author,Book
+
+@pytest.fixture
+def sample_author():
+    # Фикстура создания автора
+    return Author.objects.create(name='Тестовый Автор')
+
+@pytest.fixture
+def sample_book(author):
+    return Book.objects.create(title='Горе от ума',author=author)
+
+@pytest.fixture
+# Фикстура с несколькими авторами
+def multiple_authors():
+    authors = []
+    for i in range(10):
+        author = Author.objects.create(name=f'Автор: {i}')
+        authors.append(author)
+
+    return authors
+
+@pytest.fixture
+# Фикстура автора с несколькими книгами
+def author_with_books():
+    author = Author.objects.create(name='Шухрат')
+    for i in range(10):
+        Book.objects.create(title=f'Книга: {i}',author=author)
+    return author
