877f24d705133a784fb41901e96b3a9e5c59dd53
1-Soul-1 <ruslanpupkin789@gmail.com>
2026-01-28 11:44:54 +0500

    create author and book

---
+++ config/config/book/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Authors(models.Model):
+    name = models.CharField(max_length=100)
+    count_book =models.
+
+    def __str__(self):
+        return.self.name
+
+
+    
