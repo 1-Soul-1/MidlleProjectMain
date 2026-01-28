6f5c8fe59e2694144281fb80e190f48f6edfd63b
1-Soul-1 <ruslanpupkin789@gmail.com>
2026-01-28 12:05:02 +0500

    book-models

--- config/config/book/models.py
+++ config/config/book/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Authors(models.Model):
     name = models.CharField(max_length=100)
-    count_book =models.
+    count_book =models.PositiveIntegerField
 
     def __str__(self):
         return.self.name
